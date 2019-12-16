4fdccad9f70c46f9504ce6efd1f5ca2523493205
MeGaLiGhT14 <sasha.bodrov.99@yandex.ru>
2019-12-16 22:02:16 +0300

    Создание базовой механики

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField] GameObject _player;
+
+    private void Update()
+    {
+        transform.position = _player.transform.position + new Vector3(0 , 2 , -10);
+    }
+}
---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    [SerializeField] private GameObject _platformPrefab;
+
+    private List<GameObject> _platforms = new List<GameObject>();
+
+    private int _counterOfCreatoedPlatform = 0;
+
+    #region MonoBehaviour
+
+    private void OnValidate()
+    {
+        if (!_platformPrefab.CompareTag("Platform"))
+            _platformPrefab = null;
+    }
+
+    private void Start()
+    {
+        CreatePlatform();
+        CreatePlatform();
+    }
+
+    #endregion
+
+    public void TranspositionPlatform()
+    {
+        CreatePlatform();
+
+        if (_counterOfCreatoedPlatform > 3)
+        {
+            Destroy(_platforms[0].gameObject);
+            _platforms.RemoveAt(0);
+        }
+    }
+
+    private void CreatePlatform()
+    {
+        _platforms.Add(Instantiate(_platformPrefab , _counterOfCreatoedPlatform * Vector3.right * 50 ,
+                                                                                Quaternion.identity , transform));
+
+        _counterOfCreatoedPlatform++;
+    }
+}
